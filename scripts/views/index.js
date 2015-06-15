d17acae4e1df0f5d0b04a7ad9a8d98e68b909240
Jacob Smith <jacob@jacobsmith.io>
2015-06-15 12:53:44 -0400

    Store ID in DOM to log item

--- scripts/views/index.js
+++ scripts/views/index.js
@@ -1,10 +1,20 @@
 export default Backbone.View.extend({
   template: JST.index,
+
+  events: {
+    'click .compare': 'compareBeverage'
+  },
+
   initialize: function(){
     this.render();
   },
 
   render: function(){
     this.$el.html(this.template(this.collection.toJSON()));
+  },
+
+  compareBeverage: function(e) {
+    var id = $(e.target).data('id');
+    console.log(this.collection.get(id));
   }
 });
